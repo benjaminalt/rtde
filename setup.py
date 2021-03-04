07524744060c466665314529d683ac776bb11d32
Benjamin Alt <benjamin.alt@artiminds.com>
2021-03-04 22:42:38 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rtde',
+    version='1.0',
+    packages=['rtde', 'examples'],
+    url='git@github.com:benjaminalt/rtde.git',
+    author='Benjamin Alt',
+    author_email='benjamin.alt@artiminds.com',
+    description=''
+)
